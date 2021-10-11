4bc454799de01592a552d278de55687e58dc1e41
malasitan <malasitan1@gmail.com>
2021-10-11 23:54:08 +1100

    Streamy Video

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,41 @@
+import { map } from "lodash";
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-    return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
 
-export default StreamList;
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        console.log(this.props.streams);
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    // This going to take an object as an argument, all the different values inside of that object are going to ve pulled out and then inserted into an array
+    return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAMS:
+        // The second '...' means take whatever key-value pair we get from payload and add them to new object
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
+        case FETCH_STREAM:
+            // [] here is not an array, it's refer to un-hardcode key
+            return { ...state, [action.payload.id]: action.payload };
+        // case CREATE_STREAM:
+        //     return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            // In DELETE_STREAM, payload is id itself 
+            return _.omit(state, action.paylaod);
+        default:
+            return state;
+    }
+}
